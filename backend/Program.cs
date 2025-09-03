2b3763f8bfce4334996b34bd7478e0e7a49a57e1
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2025-09-03 21:18:15 +0800

    API connections to DB

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,14 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ExpenseDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ExpenseDb")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
