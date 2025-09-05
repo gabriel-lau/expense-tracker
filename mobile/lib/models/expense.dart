4f1079b671d2a05c51ae85ed2e32930b5a902028
gabriellau- <40619564+gabriel-lau@users.noreply.github.com>
2025-09-05 16:27:16 +0800

    use iso string method

--- mobile/lib/models/expense.dart
+++ mobile/lib/models/expense.dart
@@ -21,21 +21,11 @@ class Expense {
   }
 
   Map<String, dynamic> toJson() {
-    // Manually format to ISO8601 without milliseconds
-    String twoDigits(int n) {
-      if (n >= 10) return "${n}";
-      return "0${n}";
-    }
-
-    final utc = date.toUtc();
-    final isoString =
-        '${utc.year}-${twoDigits(utc.month)}-${twoDigits(utc.day)}T'
-        '${twoDigits(utc.hour)}:${twoDigits(utc.minute)}:${twoDigits(utc.second)}Z';
     return <String, dynamic>{
       if (id != null) 'id': id,
       'description': description,
       'amount': amount,
-      'date': isoString,
+      'date': date.toUtc().toIso8601String(),
     };
   }
 }
